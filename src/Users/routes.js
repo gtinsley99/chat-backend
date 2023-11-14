9e93158cb8f65df61b19c3b36cd012dcb86e88ae
George Tinsley <141220209+gtinsley99@users.noreply.github.com>
2023-11-14 17:11:12 +0000

    add middleware passcheck, token check, hash password

---
+++ src/Users/routes.js
@@ -0,0 +1,8 @@
+const {Router} = require("express");
+const userRouter = Router();
+
+const {} = require("./controllers");
+
+// userRouter.post("/user/adduser", addUser)
+
+module.exports = userRouter;
--- src/server.js
+++ src/server.js
@@ -7,12 +7,20 @@ const cors = require("cors");
 
 const app = express();
 
+// Models
+const User = require("./Users/model");
+
+// Routes
+const userRouter = require("./Users/routes");
+
 // Specify the port the server will listen to
 const port = process.env.PORT || 5001;
 
 // app.use is for middleware
 app.use(cors());
 app.use(express.json());
+
+app.use(userRouter);
 
 // Health check for API - see if server is working
 app.use("/health", (req, res) => {
